6bd4f4cd7ae4ec7d9d8974e1e9611f03e7d36bd6
Ldv236 <Ldv236@yandex.ru>
2023-05-08 19:16:24 +0200

    Initial commit

---
+++ src/main/java/pro/sky/c2hw6/C2hw6Application.java
@@ -0,0 +1,13 @@
+package pro.sky.c2hw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2hw6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2hw6Application.class, args);
+	}
+
+}
