43e5feb2e150e1bd568e48182609ea1f14f4fbc0
Ldv236 <Ldv236@yandex.ru>
2023-05-17 22:53:24 +0200

    hw map

--- src/main/java/pro/sky/c2hw6/service/EmployeeService.java
+++ src/main/java/pro/sky/c2hw6/service/EmployeeService.java
@@ -2,11 +2,11 @@ package pro.sky.c2hw6.service;
 
 import pro.sky.c2hw6.entity.Employee;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface EmployeeService {
     Employee addEmployee(String firstName, String lastName);
     Employee removeEmployee(String firstName, String lastName);
     Employee findEmployee(String firstName, String lastName);
-    List<Employee> findAllEmployees();
+    Collection<Employee> findAllEmployees();
 }
